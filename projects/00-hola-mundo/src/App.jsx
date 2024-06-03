09758288f703b38c5cc7c8f5c0ce761b92ededbe
theclich2 <diegobenitomartinez@gmail.com>
2024-06-03 18:04:29 +0200

    props

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,19 +1,25 @@
-import './App.css'
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard.jsx';
 export function App () {
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-header'>
-                <img className='tw-followCard-avatar' alt= " El avatar de un random"src="https://pbs.twimg.com/profile_images/1584600373922131968/QGBBlCbu_400x400.jpg"></img>
-                <div className='tw-followCard-info'>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
-                </div>
-            </header>
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+    
+    return(
+        <section className='App'>
+            <TwitterFollowCard 
+                userName="midudev" 
+                name="Miguel Ángel Durán" 
+                isFollowing={true}/>
+            <TwitterFollowCard 
+                userName="theclich2" 
+                name="diegobenito._04" 
+                isFollowing={false}/>
+            <TwitterFollowCard 
+                userName="Jose_RamonTP" 
+                name="José Ramón" 
+                isFollowing={true}/>
+            <TwitterFollowCard 
+                userName="alejxndraruiz05" 
+                name="Alejandra" 
+                isFollowing={true}/>
+        </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,5 +1,24 @@
-export function TwitterFollowCard () {
+export function TwitterFollowCard ({userName, name, isFollowing }) {
+   
     return(
-        
+            <article className='tw-followCard'>
+                <header className='tw-header'>
+                    <img 
+                        className='tw-followCard-avatar' 
+                        alt= " El avatar de un random"
+                        src={'https://unavatar.io/{userName}'} />
+                    
+                    <div className='tw-followCard-info'>
+                        <strong>{name}</strong>
+                        <span className='tw-followCard-infoUserName'>@{userName}</span>
+                    </div>
+                </header>
+
+                <aside>
+                    <button className='tw-followCard-button'>
+                        Seguir
+                    </button>
+                </aside>
+            </article>
     )
 }
