d0fe35e5ea11b649bc0222c7b6bc3acc39a9dfd4
theclich2 <diegobenitomartinez@gmail.com>
2024-06-08 15:04:19 +0200

    3

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,12 +1,73 @@
+import { useEffect, useState } from 'react';
+const FollowMouse = () =>{
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+ // pointer move
+  useEffect(() => {
+    console.log('effect', { enabled });
 
-import './App.css'
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log('handleMove', { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
 
-function App() {
-  
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+    // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
 
+    
+    return () => { // cleanup method
+      if (enabled) {
+        window.removeEventListener('pointermove', handleMove);
+      }
+    };
+  }, [enabled]);
+
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
+ useEffect(() =>{
+    document.body.classList.toggle('no-cursor', enabled)
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+ },[enabled])
   return (
-    <h1>Proyecto 3</h1>
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  );
+}
+function App() {
+ 
+  return(
+    <main>
+       <FollowMouse /> 
+    </main>
   )
 }
 
-export default App
+export default App;
